36e91ee76d663375b5767754dff5dfad411c8fbb
RaSan147 <34002411+RaSan147@users.noreply.github.com>
2023-12-20 05:41:50 +0600

    fix proxy

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ love_it_star_it = '''Love It? Star It! ⭐ https://github.com/omkarcloud/outlook
 with open('proxies.txt', 'r') as f:
 	proxies = f.read().splitlines()
 
+proxies = [f'http://{proxy}' for proxy in proxies]
 count = int(input('How many accounts do you want to create? '))
 
 Outlook.create_accounts(count=count, proxies=proxies)
