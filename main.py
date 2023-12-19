62a0877994c06a35e564fad0e595a6df38e6b792
RaSan147 <34002411+RaSan147@users.noreply.github.com>
2023-12-20 04:57:50 +0600

    add proxy

--- main.py
+++ main.py
@@ -2,4 +2,9 @@ from src import Outlook
 
 love_it_star_it = '''Love It? Star It! ⭐ https://github.com/omkarcloud/outlook-account-generator/'''
 
-Outlook.create_accounts()
+with open('proxies.txt', 'r') as f:
+	proxies = f.read().splitlines()
+
+count = int(input('How many accounts do you want to create? '))
+
+Outlook.create_accounts(count=count, proxies=proxies)
